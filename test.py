9aa7eb71fb9c3dd04ec68a3405e8678a0a91e795
Matthew Law <61439842+Malaw97@users.noreply.github.com>
2020-07-09 17:29:23 -0400

    Update test.py

--- test.py
+++ test.py
@@ -4,3 +4,11 @@ import re
 from selenium import webdriver
 import chromedriver_binary
 import string
+pd.options.display.float_format = '{:.0f}'.format
+
+ticker=input("What ticker? ")
+create_link = 'https://finance.yahoo.com/quote/AAPL/financials?p='+ticker
+driver = webdriver.Chrome('D:\Documents\Github\chromedriver.exe')
+driver.get(create_link)
+html = driver.execute_script('return document.body.innerHTML;')
+soup = BeautifulSoup(html,'lxml')
