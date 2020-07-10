be586cd9161b9b9953534ad12a11dfe022d98a84
Matthew Law <61439842+Malaw97@users.noreply.github.com>
2020-07-09 23:01:31 -0400

    First Working Push

--- test.py
+++ test.py
@@ -6,9 +6,36 @@ import chromedriver_binary
 import string
 pd.options.display.float_format = '{:.0f}'.format
 
-ticker=input("What ticker? ")
-create_link = 'https://finance.yahoo.com/quote/AAPL/financials?p='+ticker
-driver = webdriver.Chrome('D:\Documents\Github\chromedriver.exe')
-driver.get(create_link)
-html = driver.execute_script('return document.body.innerHTML;')
-soup = BeautifulSoup(html,'lxml')
+#State your designated ticker
+tickers=input("What ticker would you like to explore? (If entering more than one, separate each ticker by a comma)")
+
+def parse_input(ticker_list):
+    ticks=[]
+    ticker_list=ticker_list.split(',')
+    for i in ticker_list:
+        ticks.append(i.strip())
+    return ticks
+
+def return_data(ticker):
+    '''
+    Return data associated with a given ticker
+    '''
+    #Open chromedriver.exe
+    webdrive=webdriver.Chrome(r'D:\Documents\Github\chromedriver.exe')
+    #Assign designated ticker to hyperlink
+    webdrive.get('https://finance.yahoo.com/quote/'+ticker+'/financials?p='+ticker)
+    html = webdrive.execute_script('return document.body.innerHTML;')
+    soup = BeautifulSoup(html,'lxml')
+    Price = [i.text for i in soup.find_all('span', {'class':'Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)'})]
+    DoD_Change = [i.text for i in soup.find_all('span', {'class':'Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($positiveColor)'})]
+
+    data=[Price,DoD_Change]
+    return data
+
+def main():
+    l=[]
+    for i in parse_input(tickers):
+        l.append(return_data(i))
+    return l
+print(main())
+#print(return_data('tsla'))
